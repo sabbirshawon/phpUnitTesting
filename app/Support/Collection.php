37262f603671c60a38db28b5fa0cd5737762a444
sabbirshawon <sabbirs93@gmail.com>
2019-10-24 15:03:30 +0600

    collection class

--- app/Support/Collection.php
+++ app/Support/Collection.php
@@ -2,11 +2,14 @@
 
 namespace App\Support;
 
-class Collection
+use IteratorAggregate;
+use ArrayIterator;
+
+class Collection implements \IteratorAggregate
 {
     protected $items = [];
 
-    public function __construct(array $items)
+    public function __construct(array $items = [])
     {
         $this->items = $items;
     }
@@ -22,6 +25,25 @@ class Collection
     public function count()
     {
         return count($this->items);
+    }
+
+    public function add(array $items)
+    {
+        $this->items = array_merge($this->items, $items);
+    }
+    public function merge(Collection $collection)
+    {
+        return $this->add($collection->get());
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this->items);
+    }
+
+    public function toJson()
+    {
+        return json_encode($this->items);
     }
 
 }
--- tests/unit/CollectionTest.php
+++ tests/unit/CollectionTest.php
@@ -28,4 +28,78 @@ class CollectionTest extends TestCase
         $this->assertEquals(3, $collection->count());
 
     }
+
+    /** @test */
+    public function items_returned_match_items_passed_in()
+    {
+         
+        $collection = new \App\Support\Collection([
+            'one', 'two'
+        ]);
+
+        $this->assertCount(2, $collection->get());
+        $this->assertEquals($collection->get()[0], 'one');
+        $this->assertEquals($collection->get()[1], 'two');
+    }
+
+    /** @test */
+    public function collection_is_instance_of_iterator_aggregate()
+    {
+        $collection = new \App\Support\Collection();
+        $this->assertInstanceOf(IteratorAggregate::class, $collection);
+    }
+
+    /** @test */
+    public function collection_can_be_iterated()
+    {
+        $collection = new \App\Support\Collection([
+            'one', 'two', 'three'
+        ]);
+
+        $items = [];
+
+        foreach($collection as $item){
+            $items[] = $item;
+        }
+
+        $this->assertCount(3, $items);
+        $this->assertInstanceOf(ArrayIterator::class, $collection->getIterator());
+    }
+
+    /** @test */
+    public function collection_can_be_merged_with_another_collection()
+    {
+        $collection1 = new \App\Support\Collection(['one', 'two']);
+        $collection2 = new \App\Support\Collection(['three', 'four', 'five']);
+
+        $collection1->merge($collection2);
+
+        $this->assertCount(5, $collection1->get());
+        $this->assertEquals(5, $collection1->count());
+    }
+
+    /** @test */
+    public function can_add_to_existing_collection()
+    {
+        $collection = new \App\Support\Collection(['one', 'two']);
+
+        $collection->add(['three']);
+
+        $this->assertEquals(3, $collection->count());
+        $this->assertCount(3, $collection->get());
+    }
+
+
+    /** @test */
+    public function returns_json_encoded_items()
+    {
+        $collection = new \App\Support\Collection([
+            ['username' => 'sabbir'],
+            ['username' => 'shawon'],
+        ]);
+
+        $this->assertInternalType('string', $collection->toJson());
+        $this->assertEquals('[{"username":"sabbir"},{"username":"shawon"}]', $collection->toJson());
+    }
+
 }
